1f8d8a2ffb62979dc35b1d6c4980e3013c0711d4
qwerty-asdfg-z <75167037+qwerty-asdfg-z@users.noreply.github.com>
2022-06-12 15:54:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,88 @@
-Webcam.set({
-    width:360,
-    height:250,
-    image_format : 'jpeg',
-    jpeg_quality:90
-});
-camera = document.getElementById("camera");
-Webcam.attach("#camera");
-function take_snapshot()
+quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye","eyeglasses","face","fan","feather","fence","finger","fire hydrant","fireplace","firetruck","fish","flamingo","flashlight","flip flops","floor lamp","flower","flying saucer","foot","fork","frog","frying pan","garden","garden hose","giraffe","goatee","golf club","grapes","grass","guitar","hamburger","hammer","hand","harp","hat","headphones","hedgehog","helicopter","helmet","hexagon","hockey puck","hockey stick","horse","hospital","hot air balloon","hot dog","hot tub","hourglass","house","house plant","hurricane","ice cream","jacket","jail","kangaroo","key","keyboard","knee","knife","ladder","lantern","laptop","leaf","leg","light bulb","lighter","lighthouse","lightning","line","lion","lipstick","lobster","lollipop","mailbox","map","marker","matches","megaphone","mermaid","microphone","microwave","monkey","moon","mosquito","motorbike","mountain","mouse","moustache","mouth","mug","mushroom","nail","necklace","nose","ocean","octagon","octopus","onion","oven","owl","paintbrush","paint can","palm tree","panda","pants","paper clip","parachute","parrot","passport","peanut","pear","peas","pencil","penguin","piano","pickup truck","picture frame","pig","pillow","pineapple","pizza","pliers","police car","pond","pool","popsicle","postcard","potato","power outlet","purse","rabbit","raccoon","radio","rain","rainbow","rake","remote control","rhinoceros","rifle","river","roller coaster","rollerskates","sailboat","sandwich","saw","saxophone","school bus","scissors","scorpion","screwdriver","sea turtle","see saw","shark","sheep","shoe","shorts","shovel","sink","skateboard","skull","skyscraper","sleeping bag","smiley face","snail","snake","snorkel","snowflake","snowman","soccer ball","sock","speedboat","spider","spoon","spreadsheet","square","squiggle","squirrel","stairs","star","steak","stereo","stethoscope","stitches","stop sign","stove","strawberry","streetlight","string bean","submarine","suitcase","sun","swan","sweater","swingset","sword","syringe","table","teapot","teddy-bear","telephone","television","tennis racquet","tent","The Eiffel Tower","The Great Wall of China","The Mona Lisa","tiger","toaster","toe","toilet","tooth","toothbrush","toothpaste","tornado","tractor","traffic light","train","tree","triangle","trombone","truck","trumpet","tshirt","umbrella","underwear","van","vase","violin","washing machine","watermelon","waterslide","whale","wheel","windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"]
+
+random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+console.log(quick_draw_data_set[random_number]);
+sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
+
+function updateCanvas() {
+  background("white");
+  random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+  console.log(quick_draw_data_set[random_number]);
+  sketch = quick_draw_data_set[random_number];
+  document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
+
+function preload() {
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+}
+
+
+function draw() {
+  // Set stroke weight to 10
+  strokeWeight(13);
+  // Set stroke color to black
+  stroke(0);
+  // If mouse is pressed, draw line between previous and current mouse positions
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+
+  check_sketch()
+  if(drawn_sketch == sketch)
+  {
+    answer_holder = "set"
+    score++;
+    document.getElementById('score').innerHTML = 'Score: ' + score;
+  }
+
+}
+
+function classifyCanvas() {
+  classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results);
+  drawn_sketch = results[0].label;
+  document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+
+  document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+}
+
+
+function check_sketch()
 {
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
-}
-console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/sBh2fve97/model.json',modelLoaded)
-
-function modelLoaded(){
-    console.log('Model Loaded!');
-}
-function check(){
-    img=document.getElementById('captured_image');
-    classifier.classify(img,gotresult)
-}
-function speak() {
-    var synt=window.speechSynthesis;
-    speak_data_1="The first prediction is"+prediction_1;
-    speak_data_2="The second prediction is"+prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
-    synt.speak(utterThis);
-
-}
-function gotresult(error,results){
-    if (error) {
-        console.error(error);
-    }else{
-        console.log(results);
-        document.getElementById("result_emotion_name").innerHTML=results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
-        prediction_1=results[0].label;
-        prediction_2=results[1].label;
-        speak();
-        if(results[0].label=='Amzing')
-        {document.getElementById("update_emoji").innerHTML="&#128522";
+  timer_counter++;
+  document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+  console.log(timer_counter)
+  if(timer_counter > 400)
+    {
+      timer_counter = 0;
+      timer_check = "completed"
     }
-    if(results[0].label=='Best')
-    {document.getElementById("update_emoji").innerHTML="&#128532";
-    
-}
-if(results[0].label=='Victory')
-    {document.getElementById("update_emoji").innerHTML="&#128548";
-    
+    if(timer_check =="completed" || answer_holder == "set")
+    {
+      timer_check = "";
+      answer_holder = "";
+      updateCanvas();
     }
-    if(results[1].label=='Amzing')
-    {document.getElementById("update_emoji2").innerHTML="&#128522";
-}
-if(results[1].label=='Best')
-{document.getElementById("update_emoji2").innerHTML="&#128532";
-
-}
-if(results[1].label=='Victory')
-{document.getElementById("update_emoji2").innerHTML="&#128548";
 
 }
 
-    }
-}
